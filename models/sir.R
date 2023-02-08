06c535e3ff37ea24fd5b1f3908e3aff33ca8a6e0
Rich FitzJohn <rich.fitzjohn@gmail.com>
2023-02-08 19:25:59 +0000

    start on code

--- models/sir.R
+++ models/sir.R
@@ -4,12 +4,14 @@ p_IR <- 1 - exp(-(gamma))
 n_IR <- rbinom(I, p_IR * dt)
 n_SI <- rbinom(S, p_SI * dt)
 
+update(time) <- (step + 1) * dt
 update(S) <- S - n_SI
 update(I) <- I + n_SI - n_IR
 update(R) <- R + n_IR
 update(cases_cumul) <- cases_cumul + n_SI
 update(cases_inc) <- if (step %% freq == 0) n_SI else cases_inc + n_SI
 
+initial(time) <- 0
 initial(S) <- 1000
 initial(R) <- 0
 initial(I) <- I0
@@ -20,5 +22,5 @@ beta <- user(0.2)
 gamma <- user(0.1)
 I0 <- user(10)
 
-freq <- 4
+freq <- user(4)
 dt <- 1.0 / freq
